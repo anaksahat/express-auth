2b92e50a06cb380af666a6e99e6bb7628651360c
Ahmad Thomthomi Barosimi <tommy.ethan.walter@gmail.com>
2024-05-17 15:04:21 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const errorhanlder = require("./middleware/errorHandler");
+const logger = require("./middleware/logger");
+const userRoutes = require("./routes/userRoutes");
+const { BASE_URL, PORT } = require("./config/appConfig");
+const authRoutes = require("./routes/authRoutes");
+const authMiddleware = require("./middleware/authMiddleware");
+
+// Middleware
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(errorhanlder);
+app.use(logger);
+app.use("/api/auth", authRoutes);
+
+// HANYA USER YANG LOGIN BISA CRUD data users
+app.use(authMiddleware);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => console.log(`Server is running on ${BASE_URL}:${PORT}`));
