77d0c0e221b5c5dd86ba05c589f21078cd846674
Ahmad Thomthomi Barosimi <tommy.ethan.walter@gmail.com>
2024-06-04 19:24:17 +0700

    fix bugs on middleware and errorhandler

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,8 @@ const database = require("../model/database");
 
 const authMiddleware = async (req, res, next) => {
   // AMBIL TOKEN DARI request headers
-  const token = req.headers.authorization.split(" ")[1];
+  console.log(req.headers?.authorization);
+  const token = req.headers?.authorization?.split(" ")[1];
 
   if (token === null)
     return res.status(401).json({ message: "Unauthorized: Token missing" });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const errorhanlder = require("./middleware/errorHandler");
+const errorhandler = require("./middleware/errorHandler");
 const logger = require("./middleware/logger");
 const userRoutes = require("./routes/userRoutes");
 const { BASE_URL, PORT } = require("./config/appConfig");
@@ -9,16 +9,23 @@ const authMiddleware = require("./middleware/authMiddleware");
 const cors = require("cors");
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(errorhanlder);
 app.use(logger);
 app.use("/api/auth", authRoutes);
 
 // HANYA USER YANG LOGIN BISA CRUD data users
-// app.use(authMiddleware);
+app.use(authMiddleware);
 app.use("/api/users", userRoutes);
+
+// error handler execute at the end
+app.use("/*", (req, res) =>
+  res.status(404).json({
+    error: "ERR NOT FOUND",
+  })
+);
+app.use(errorhandler);
 
 app.listen(PORT, () => console.log(`Server is running on ${BASE_URL}:${PORT}`));
